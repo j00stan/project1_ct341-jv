f781321eccfc63ba453acc6fab62981621a6596a
j00stan <98915813+j00stan@users.noreply.github.com>
2022-03-21 20:00:25 -0400

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,31 @@
 console.log(typeof $);
 
-$(".hero-content").fadeIn(2500);
+const faders = document.querySelectorAll(".fade");
+const sliders = document.querySelectorAll (".slide")
+
+const appearOptions = {
+    threshold: 0
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
